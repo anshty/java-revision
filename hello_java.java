123551818560d1b3bf4238ae758eb62eb194be10
ankit <ankitverma374627@gmail.com>
2023-04-06 21:57:31 +0530

    first pattern

--- hello_java.java
+++ hello_java.java
@@ -1,5 +1,6 @@
 public class hello_java {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello i am ansh\n");
+        System.out.println("");
     }
 }
---
+++ patten_01.java
@@ -0,0 +1,5 @@
+public class patten_01 {
+    public static void main(String[] args) {
+        System.out.println("*\n* *\n* * *\n* * * *");
+    }
+}
