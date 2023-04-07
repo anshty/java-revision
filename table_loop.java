a51a6658914ca2b68eacf82337d7a2a8e08610f8
ankit <ankitverma374627@gmail.com>
2023-04-07 23:31:01 +0530

    table

---
+++ table_loop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class table_loop {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+        System.out.println("enter no:");
+        int tb=sc.nextInt();
+        int i=1;
+        for(i=1;i<=10;i++){
+            System.out.println(tb*i);
+        }
+    }
+}
