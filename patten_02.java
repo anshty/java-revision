3d86262fe91c80f2cf6c7f2a04742f5c399fc604
ankit <ankitverma374627@gmail.com>
2023-04-08 23:29:26 +0530

    pattern

---
+++ patten_02.java
@@ -0,0 +1,13 @@
+public class patten_02 {
+    public static void main(String[] args) {
+        int i=1;
+        for(i=1;i<=4;i++){
+            //System.out.println("#");
+            int j=1;
+            for(j=1;j<=5 ;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
